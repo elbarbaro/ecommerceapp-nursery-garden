fd18286d6b1b9d27b5f5858a15c8de6dcf3f42b0
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2022-03-08 20:21:49 -0600

    initial commit

---
+++ src/main/java/com/nurserygarden/ecommerceapp/EcommerceAppNurseryGardenApplication.java
@@ -0,0 +1,13 @@
+package com.nurserygarden.ecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAppNurseryGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAppNurseryGardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nurserygarden/ecommerceapp/EcommerceAppNurseryGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurserygarden.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppNurseryGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
