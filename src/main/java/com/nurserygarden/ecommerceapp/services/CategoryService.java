440413a86175983046b3cc8e4c71e202581b983c
Leo López <leopoldo.misael.lopez.de.alba@gmail.com>
2023-11-06 21:36:26 -0600

    resolve conflicts

--- src/main/java/com/nurserygarden/ecommerceapp/services/CategoryService.java
+++ src/main/java/com/nurserygarden/ecommerceapp/services/CategoryService.java
@@ -4,8 +4,12 @@ import com.nurserygarden.ecommerceapp.controllers.requests.CategoryDto;
 import com.nurserygarden.ecommerceapp.controllers.responses.CategoryResponse;
 import com.nurserygarden.ecommerceapp.repositories.entities.Category;
 
+import java.util.List;
+
 public interface CategoryService {
     CategoryResponse create(CategoryDto category);
+
+    List<CategoryResponse> findAll();
 
     Category findById(Long id);
 }
