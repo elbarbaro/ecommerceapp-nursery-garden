5f2050f11b987ffd537c839575872f82e4dba5c7
Raisa Toscano <daisa1690@gmail.com>
2024-01-18 00:14:52 -0600

    Add CloudConfig, update FileStoreServiceImpl to use CloudConfig add uploadFileToS3

--- src/main/java/com/nurserygarden/ecommerceapp/controllers/ProductImageController.java
+++ src/main/java/com/nurserygarden/ecommerceapp/controllers/ProductImageController.java
@@ -19,14 +19,12 @@ public class ProductImageController {
 
     private final static Logger log = LoggerFactory.getLogger(ProductImageController.class);
 
-
     private FileStoreServiceImpl fileStoreService;
 
-    public ProductImageController( FileStoreServiceImpl fileStoreService){
-        this.fileStoreService= fileStoreService;
+    public ProductImageController(FileStoreServiceImpl fileStoreService) {
+
+        this.fileStoreService = fileStoreService;
     }
-
-
 
     @PostMapping(path = "/products/{id}/images", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<String> saveImage(@RequestParam("image") @RequestPart MultipartFile document) throws Exception {
--- src/main/java/com/nurserygarden/ecommerceapp/filestore/FileStoreServiceImpl.java
+++ src/main/java/com/nurserygarden/ecommerceapp/filestore/FileStoreServiceImpl.java
@@ -1,32 +1,55 @@
 package com.nurserygarden.ecommerceapp.filestore;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Service
 public class FileStoreServiceImpl {
 
-    @Value("${aws.access.key}")
-    private String AWS_ACCESS_KEY;
-    @Value("${aws.secret.key}")
-    private String AWS_SECRET_KEY;
+    private AmazonS3 awsClient;
+
+    private FileStoreServiceImpl(AmazonS3 awsClient) {
+        this.awsClient = awsClient;
+    }
+
     @Value("${aws.bucket}")
-    private String AWS_BUCKET;
+    public String AWS_BUCKET;
 
     public String uploadMultipartFileS3(MultipartFile image) {
-        AwsCloudUtil util = new AwsCloudUtil();
 
         try {
-            util.uploadFileToS3(image.getOriginalFilename(), image, AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_BUCKET);
+            uploadFileToS3(image.getOriginalFilename(), image, AWS_BUCKET);
+
             return String.format("File %s uploaded succesfully", image.getOriginalFilename());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return String.format("File %s uploadeded failed", image.getOriginalFilename());
+    }
+
+    public void uploadFileToS3(String fileName, MultipartFile image, String bucket) throws IOException {
+
+        File file = new File(fileName);
+
+        try (OutputStream os = new FileOutputStream(file)) {
+            os.write(image.getBytes());
+
+            awsClient.putObject(bucket, fileName, file);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
